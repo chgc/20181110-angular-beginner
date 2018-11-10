06aaf13950dd61a1eb8c66f6624f54e4655b88e5
Kevin Yang <chgc_kevin@hotmail.com>
2018-11-10 15:06:21 +0800

    新增 removeTodo 按鈕功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,4 +34,13 @@ export class AppComponent {
   completeAll() {
     this.todos.forEach(todo => (todo.isCompleted = true));
   }
+
+  removeTodo(idx, todo) {
+    // 方法1
+    // this.todos.splice(this.todos.indexOf(todo), 1);
+    // 方法2
+    // this.todos = this.todos.filter(_todo => _todo !== todo);
+    // 方法3
+    // this.todos.splice(idx, 1);
+  }
 }
